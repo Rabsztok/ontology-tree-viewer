b3e529da6daf472a31340a63e0f9ee40334e11c5
rabsztok <rabsztok@gmail.com>
2018-10-02 18:36:08 +0200

    Configure build process for heroku; fix tests

--- src/components/node/node.test.js
+++ src/components/node/node.test.js
@@ -4,18 +4,18 @@ import { Node } from './node'
 
 describe('Node', () => {
   it('renders branch without errors', () => {
-    const element = { name: 'branch', active: false, children: [{name: 'child'}] }
-    const component = shallow(
-      <Node element={element}/>
-    )
+    const element = {
+      name: 'branch',
+      active: false,
+      children: [{ id: 2, name: 'child' }]
+    }
+    const component = shallow(<Node element={element} />)
     expect(component).toMatchSnapshot()
   })
 
   it('renders leaf without errors', () => {
     const element = { name: 'branch', active: false, toggle: () => {} }
-    const component = shallow(
-      <Node element={element}/>
-    )
+    const component = shallow(<Node element={element} />)
     expect(component).toMatchSnapshot()
   })
 })
